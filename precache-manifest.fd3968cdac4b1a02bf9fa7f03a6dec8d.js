ac20bfaac1bc58f263e9b8d18499512ef6431dca
bythealphabet <bythealphabet@gmail.com>
2019-09-16 13:34:10 -0400

    Updates

---
+++ precache-manifest.fd3968cdac4b1a02bf9fa7f03a6dec8d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ba98f928ab1a8138b176c0a89d154d",
+    "url": "/Jacqueline-Daflaar/index.html"
+  },
+  {
+    "revision": "aebfbdb3c82e14f18e07",
+    "url": "/Jacqueline-Daflaar/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "1b2548eec5c1446dd8dd",
+    "url": "/Jacqueline-Daflaar/static/css/main.41f227d7.chunk.css"
+  },
+  {
+    "revision": "aebfbdb3c82e14f18e07",
+    "url": "/Jacqueline-Daflaar/static/js/2.48335d60.chunk.js"
+  },
+  {
+    "revision": "1b2548eec5c1446dd8dd",
+    "url": "/Jacqueline-Daflaar/static/js/main.cacd6559.chunk.js"
+  },
+  {
+    "revision": "eed20e14d84f545971df",
+    "url": "/Jacqueline-Daflaar/static/js/runtime~main.773081fb.js"
+  },
+  {
+    "revision": "8500e862a7e966c3ed7fd791f01b6696",
+    "url": "/Jacqueline-Daflaar/static/media/Bob.8500e862.jpg"
+  },
+  {
+    "revision": "16038acda389f98d172c947fe350dfd8",
+    "url": "/Jacqueline-Daflaar/static/media/award.16038acd.jpg"
+  },
+  {
+    "revision": "1289a125963dc59b65e253c5fbfeb0d4",
+    "url": "/Jacqueline-Daflaar/static/media/bckball.1289a125.jpg"
+  },
+  {
+    "revision": "b208c888dbff7ff9611e7db67012ec54",
+    "url": "/Jacqueline-Daflaar/static/media/img1.b208c888.jpg"
+  },
+  {
+    "revision": "f5dba7f0d54f5f250b31eb11409fc311",
+    "url": "/Jacqueline-Daflaar/static/media/img2.f5dba7f0.jpg"
+  },
+  {
+    "revision": "ab8e10e1986b8253bb41ac9ae0fe6583",
+    "url": "/Jacqueline-Daflaar/static/media/img3.ab8e10e1.jpg"
+  },
+  {
+    "revision": "30b2deb2d47ddb00b1e91111606cc503",
+    "url": "/Jacqueline-Daflaar/static/media/img4.30b2deb2.jpg"
+  },
+  {
+    "revision": "9859e81c0b00ef81b8af4d0178a0573c",
+    "url": "/Jacqueline-Daflaar/static/media/img5.9859e81c.jpg"
+  },
+  {
+    "revision": "d657fe13b61dadd8b315ccc5a1764633",
+    "url": "/Jacqueline-Daflaar/static/media/jacqDafTrainer.d657fe13.png"
+  },
+  {
+    "revision": "23ac5a7c88378522f95483e5e9ac44b1",
+    "url": "/Jacqueline-Daflaar/static/media/sample.23ac5a7c.mp4"
+  }
+]);
